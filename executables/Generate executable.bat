d64c4b6881cbebc5e274f8f0a8d4e6c7fa6bbed4
btakli <64867481+btakli@users.noreply.github.com>
2022-05-10 17:54:11 -0400

    Added green background text to batch file output

--- executables/Generate executable.bat
+++ executables/Generate executable.bat
@@ -1,12 +1,14 @@
 @ECHO off
 
-ECHO ~~~~~~~~Generating EXEs~~~~~~
-
-ECHO ~~~~Generating EXE for GUI version~~~~
+ECHO [42m~~~~~~~~Generating EXEs~~~~~~[0m
+ECHO [42m~~~~Generating EXE for GUI version~~~~[0m
 pyinstaller --onefile "../source code/MasterTool (GUI).spec"
 move /y ".\dist\MasterTool (GUI).exe" .\
 
-ECHO ~Generating EXE for CLI version~
+ECHO [42m~~~~Generating EXE for CLI version~~~~[0m
 pyinstaller --onefile "../source code/MasterTool.spec"
 move /y ".\dist\MasterTool.exe" .\
+
+ECHO [42m~~~~Done!~~~~[0m
+PAUSE
 
